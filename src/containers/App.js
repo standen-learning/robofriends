a1a551fd4514545f6774aa5750eb40f9a9e14318
Sean Standen <SStanden@Adm2s-MBP.attlocal.net>
2022-02-25 14:16:56 -0800

    Updated all state to be run with Redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,41 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { connect, useSelector, useDispatch } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
+import { requestRobots, setSearchField } from '../actions';
 
-function App()  {
-	const [robots, setRobots] = useState([]);
-	const [searchfield, setSearchfield] = useState('');
+function App({props})  {
+	// const [robots, setRobots] = useState([]);
+	const robotsState = useSelector(state => state.requestRobots);
+	const { robots, isPending, error } = robotsState;
+	const searchField = useSelector(state => state.searchRobots.searchField);
+	const dispatch = useDispatch();
+	const  onSearchChange = event => dispatch(setSearchField(event.target.value));
 	
 	useEffect(() => {
-		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response => response.json())
-		.then(users => {setRobots(users)})
-	}, []);
-
-	const onSearchChange = (event) => {
-		setSearchfield(event.target.value);
-	}
+		dispatch(requestRobots());
+	}, [dispatch]);
 
 	const filteredRobots = robots.filter(robot => {
-	return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+	return robot.name.toLowerCase().includes(searchField.toLowerCase());
 	})
-	return !robots.length ?
-		<h1>Loading</h1> :
-	(
-	<div className='tc'>
-		<h1 className='f2'>RoboFriends</h1>
-		<SearchBox searchChange={onSearchChange}/>
-		<Scroll>
-			<ErrorBoundry>
-				<CardList robots={filteredRobots}/>
-			</ErrorBoundry>
-		</Scroll>
-	</div>
-	)
+
+	let renderRobots;
+	if(isPending) {
+		renderRobots = <h1>Loading</h1>;
+	} else if (error) {
+		renderRobots = (
+			<div>
+				<h1>There was an error loading the robots.</h1>
+				<p>Please refresh the page to try again.</p>
+			</div>
+		);
+	} else {
+		renderRobots = (
+			<div className='tc'>
+				<h1 className='f2'>RoboFriends</h1>
+				<SearchBox searchChange={onSearchChange}/>
+				<Scroll>
+					<ErrorBoundry>
+						<CardList robots={filteredRobots}/>
+					</ErrorBoundry>
+				</Scroll>
+			</div>
+		);
+	}
+	return renderRobots
 }
 
-export default App;
+export default connect()(App);
