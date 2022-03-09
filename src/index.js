98a1485747b04d9c83f91d2a3802c2b6f48c325c
Sean Standen <standen.sean@gmail.com>
2022-03-09 13:11:20 -0800

    updates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
+//import { createLogger } from 'redux-logger';
 import ThunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App'
@@ -10,10 +10,10 @@ import * as serviceWorker from './serviceWorker';
 import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
-const logger = createLogger();
+// const logger = createLogger();
 
 const rootReducer = combineReducers({ searchRobots, requestRobots });
-const store = createStore(rootReducer, applyMiddleware(ThunkMiddleware, logger));
+const store = createStore(rootReducer, applyMiddleware(ThunkMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
