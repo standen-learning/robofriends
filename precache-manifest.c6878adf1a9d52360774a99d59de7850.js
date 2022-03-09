9ec09eea757dbbd0274a751517322ace2d9fd4f6
Sean Standen <SStanden@Adm2s-MBP.attlocal.net>
2022-03-09 14:18:32 -0800

    Updates

---
+++ precache-manifest.c6878adf1a9d52360774a99d59de7850.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7678a44b03a4d907dee1",
+    "url": "/robofriends/static/css/main.96ce06c8.chunk.css"
+  },
+  {
+    "revision": "7678a44b03a4d907dee1",
+    "url": "/robofriends/static/js/main.f3743adc.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "bb3965aa86e97006e7e5",
+    "url": "/robofriends/static/css/2.5e098c9b.chunk.css"
+  },
+  {
+    "revision": "bb3965aa86e97006e7e5",
+    "url": "/robofriends/static/js/2.433300d1.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d0d9f6538a7b12f3f2942a3869ea187c",
+    "url": "/robofriends/index.html"
+  }
+];
