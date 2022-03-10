da650a5999f496ed7f7fa6688bffc8f5e15d7aa4
Sean Standen <SStanden@Adm2s-MBP.attlocal.net>
2022-03-10 09:30:52 -0800

    Updates

--- precache-manifest.c4d1cd38861d2ac162d9c691bd829ec9.js
+++ precache-manifest.c4d1cd38861d2ac162d9c691bd829ec9.js
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
   },
   {
-    "revision": "39cdd64556136973dbe2dad7da7deb9d",
+    "revision": "5f05dd0b69379fc4cdb2579ba3eb48c1",
     "url": "/robofriends/index.html"
   }
 ];
